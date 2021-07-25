75ccfb810230b840f5b1aec0bd6a57a120b1ca47
ranjith gaddam <granjithr20@gmail.com>
2021-07-24 22:28:05 -0400

    initial commit with creat-react-app

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# nextTrip-caseStudy
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          NextTrip - Case Study
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
